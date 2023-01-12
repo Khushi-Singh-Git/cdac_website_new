b3f6d51a7b48b65406256f943ec50fab99a29087
Khushi <khushi35b@gmail.com>
2023-01-12 17:10:22 +0530

    Update Diagnose.js

--- frontend/src/component/Diagnose.js
+++ frontend/src/component/Diagnose.js
@@ -1,39 +1,155 @@
-import React from "react";
-import {Helmet} from "react-helmet";
-import "../index.css";
-const Diagnose = () => {
+import { useState, useRef } from "react";
+
+function Diagnose() {
+  const [processing, setProcessing] = useState(false);
+  const [showDiagnoseButton, setshowDiagnose] = useState(false);
+  const [showDiagnosing, setshowDiagnosing] = useState(false);
+  const [stateReport, setstateReport] = useState("");
+
+  function submitFile(event) {
+    event.preventDefault();
+    console.log("event fired");
+    console.log(event.target);
+    setProcessing(true);
+    fetch("http://localhost:4000/multiple", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST",
+        Accept: "*/*",
+        "Accept-Encoding": "gzip,br,deflate",
+      },
+      method: "POST",
+      // mode: "no-cors",
+      body: new FormData(event.target),
+    })
+      .then((response) => response.json())
+      .then((body) => {
+        console.log("her");
+        console.log(body);
+
+        //access button after files uplaoded
+      })
+      .catch((error) => {
+        // TODO handle error
+        console.log(error);
+        console.log("some error occured");
+      })
+      .finally(() => {
+        setProcessing(false);
+        setshowDiagnose(true);
+      });
+    //   .finally({ setProcessing(false),
+    //   setshowDiagnose(true);})
+    // return false;
+  }
+
+  function diagnose() {
+    setshowDiagnosing(true);
+    console.log("diagnose fired");
+    fetch("http://localhost:4000/diagnose", {
+      headers: { "Access-Control-Allow-Origin": "*" },
+      method: "GET",
+      // mode: "no-cors",
+    })
+      .then((response) => response.json())
+      .then((body) => {
+        console.log("her");
+        console.log(body);
+        setstateReport(body);
+        setshowDiagnosing(false);
+        setshowDiagnose(false);
+        //access button after files uplaoded
+      })
+      .catch((error) => {
+        // TODO handle error
+        console.log(error);
+        console.log("some error occured");
+        // setProcessing(false);
+        // setshowDiagnose(true);
+      });
+    // .finally(setshowDiagnosing(false));
+  }
+
+  const filesRef = useRef();
+
   return (
     <>
-      <div style={{ color: "white" ,}}>
-        <h4>Upload the lung sound files here:</h4>
-        <form
-          id="FormId"
-          action="/multiple-upload"
-          method="POST"
-          enctype="multipart/form-data"
-        >
-          <div class="form-group">
-            <label for="example-input-file"> </label>
+    
+      <div style={{ color: "white", fontSize: "27px",width: "750px",border: "15px solid #40739e", padding: "50px", margin: "20px" }}>
+        <h2 style={{ fontFamily: "Poppins" }}>
+          Upload the lung auscultation sound files here:
+        </h2>
+        <br></br>
+        {processing ? (
+          "Uploading..."
+        ) : (
+          <form
+            id="FormId"
+            //   action="/multiple-upload"
+            method="POST"
+            encType="multipart/form-data"
+            onSubmit={(event) => {
+              submitFile(event);
+            }}
+          >
             <input
               type="file"
-              name="multi-files"
               multiple
-              id="input-multi-files"
-              class="form-control-file border"
+              ref={filesRef}
+              name="images"
+              style={{ fontSize: "23px",fontFamily:"Poppins" }}
             />
-          </div>
-          <button type="submit" class="btn btn-primary">
-            Submit
-          </button>
-        </form>
-        <p id="response_message">No files uploaded</p>
-        <button id="python-btn" class="btn btn-primary" hidden="true">
-          Diagnose
-        </button>
-        <h4 id="output"></h4>
+            <button
+              style={{
+                fontFamily: "Poppins",
+                fontSize: "25px",
+                borderRadius: "7px",
+                backgroundColor: "#c7ecee",
+              }}
+              type="submit"
+            >
+              Submit
+            </button>
+            <br></br><br></br>
+          </form>
+   
+        )}
+        {showDiagnoseButton ? (
+          showDiagnosing ? (
+            <p style={{ fontFamily: "Poppins",textAlign:"center" }}>Diagnosing the audio files and fetching AI generated results...</p>
+          ) : (
+            <button onClick={diagnose} disabled={showDiagnosing} style={{
+              fontFamily: "Poppins",
+              fontSize: "25px",
+              borderRadius: "7px",
+              backgroundColor: "#c7ecee",
+              textAlign:"center",
+              display:"flex",
+              justifyContent:"center",
+            }}>
+              Diagnose
+            </button>
+          )
+        ) : null}
+        {/* {showDiagnoseButton ? (
+        showDiagnosing ? (
+          <p>Analysing files and conducting diagnosis</p>
+        ) : (
+          <p>hi</p>
+        )
+      ) : null} */}
+      <div style={{fontFamily:"Poppins",display:"flex",justifyContent:"center",color:"#f0932b",fontSize:"35px",fontStyle:"bold"}}>   {stateReport }</div>
+     
       </div>
-    </>
+      <br></br><br></br>
+      <hr></hr>
+      <h6
+      style={{ textAlign: "center", color: "white", fontFamily: "poppins",marginBottom:"1px",paddingBottom:"1px" }}>
+      {" "}
+      Developed by CDAC, Mohali
+    </h6></>
+  
   );
-};
+}
 
 export default Diagnose;
